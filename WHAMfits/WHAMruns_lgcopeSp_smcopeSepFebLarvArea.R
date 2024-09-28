ab47f4a4b8fa571d0c1be075afdd108687bb6ecb
Sarah Gaichas <sarah.gaichas@noaa.gov>
2024-09-27 20:00:01 -0400

    2 covariates

--- WHAMfits/WHAMruns_lgcopeSp_smcopeSepFebLarvArea.R
+++ WHAMfits/WHAMruns_lgcopeSp_smcopeSepFebLarvArea.R
@@ -28,12 +28,23 @@ input <- mm192mod$input
 # larger set of ecov setups to compare
 df.mods <- data.frame(Recruitment = c(rep(2, 16)),
                       ecov_process = c(rep("rw",8),rep("ar1",8)),
-                      ecov_how = c(rep("none",4), 
+                      ecov_how = c(rep("none" ,4), 
                                    rep("controlling-lag-0-linear",4)),
                       ecovdat = rep(c("logmean-logsig", 
                                       "logmean-est_1",
                                       "meanmil-logsigmil",
                                       "meanmil-est_1"),4),
+                      stringsAsFactors=FALSE)
+n.mods <- dim(df.mods)[1]
+df.mods$Model <- paste0("m",1:n.mods)
+df.mods <- dplyr::select(df.mods, Model, tidyselect::everything()) # moves Model to first col
+
+# make smaller
+df.mods <- data.frame(Recruitment = c(rep(2, 4)),
+                      ecov_process = c(rep(c("rw","ar"),2)),
+                      ecov_how = c(rep("none" ,2), 
+                                   rep("controlling-lag-0-linear",2)),
+                      ecovdat = rep("logmean-est_1",4),
                       stringsAsFactors=FALSE)
 n.mods <- dim(df.mods)[1]
 df.mods$Model <- paste0("m",1:n.mods)
@@ -45,6 +56,14 @@ df.mods <- dplyr::select(df.mods, Model, tidyselect::everything()) # moves Model
 lgcope.dat <- read.csv(here::here("WHAMfits/springlargecopeindex.csv"), header=T)
 
 smcope.dat <- read.csv(here::here("WHAMfits/sepfebsmallcopeALLlarvareaindex.csv"), header=T)
+
+# 2020 is missing for fall small copepods
+smcope.dat[smcope.dat == 0] <- NA
+
+# don't use the NA value
+use.obs <- matrix(1, ncol=1, nrow=dim(smcope.dat)[1])
+use.obs[39,] <- 0
+
 
 ## Run model
 
@@ -61,24 +80,28 @@ for(m in 1:n.mods){
   }else{"est_1"}
   
   smcopedat <- dplyr::case_when(df.mods$ecovdat[m] %in% c("logmean-logsig", "logmean-est_1") ~
-                                  as.matrix(log(lgcope.dat$her_larv_Estimate)),
-                                TRUE ~as.matrix(lgcope.dat$her_larv_Estimate/1000000))
+                                  as.matrix(log(smcope.dat$her_larv_Estimate)),
+                                TRUE ~as.matrix(smcope.dat$her_larv_Estimate/1000000))
   
   smcopesig <- if(df.mods$ecovdat[m] %in% c("logmean-logsig")){
-    as.matrix(log(lgcope.dat$her_larv_SE))
+    as.matrix(log(smcope.dat$her_larv_SE))
   }else if(df.mods$ecovdat[m] %in% c("meanmil-logsigmil")){
-    as.matrix(log(lgcope.dat$her_larv_SE/1000000))
+    as.matrix(log(smcope.dat$her_larv_SE/1000000))
   }else{"est_1"}
+  
+  smcopehow <- ifelse(df.mods$ecov_how[m]=="controlling-lag-0-linear", #lgcope
+                      "controlling-lag-1-linear", #if yes
+                      "none") #if no
   
   
   ecov <- list(
-    label = "lgCopeSpring2",
-    mean = ecovdat,
-    logsigma = ecovsig, 
-    year = env.dat$Time,
-    use_obs =  matrix(1, ncol=1, nrow=dim(env.dat)[1]), # use all obs (all = 1)
-    process_model = df.mods$ecov_process[m],  # "rw" or "ar1"
-    recruitment_how = as.matrix(df.mods$ecov_how[m])
+    label = c("lgCopeSpring2", "smCopeSepFeb2"),
+    mean = cbind(lgcopedat, smcopedat),
+    logsigma = cbind(lgcopesig, smcopesig), 
+    year = lgcope.dat$Time,
+    use_obs =  cbind(matrix(1, ncol=1, nrow=dim(lgcope.dat)[1]), use.obs), # use all obs (all = 1)
+    process_model = c(df.mods$ecov_process[m], df.mods$ecov_process[m]), # "rw" or "ar1"
+    recruitment_how = rbind(as.matrix(df.mods$ecov_how[m]), as.matrix(smcopehow))
   ) 
   
   ecovinput <- set_ecov(input, ecov=ecov)
@@ -93,4 +116,91 @@ for(m in 1:n.mods){
   
 }
 
+## Try just running a model with both turned off and both turned on, log scale and est_1
+## having issues getting the correct class of objects in this loop
 
+#1: rw none logmean-est1 both
+#2: ar1 none logmean-est1 both
+
+#3: rw controlling-lag-0-linear and controlling-lag-1-linear logmean-est1 both
+#4: ar1 controlling-lag-0-linear and controlling-lag-1-linear logmean-est1 both
+
+ecov1 <- list(
+  label = c("lgCopeSpring2", "smCopeSepFeb2"),
+  mean = cbind(as.matrix(log(lgcope.dat$her_sp_Estimate)), 
+               as.matrix(log(smcope.dat$her_larv_Estimate))),
+  logsigma = c("est_1", "est_1"), 
+  year = lgcope.dat$Time,
+  use_obs =  cbind(matrix(1, ncol=1, nrow=dim(lgcope.dat)[1]), use.obs), # use all obs (all = 1)
+  process_model = c("rw","rw"), # "rw" or "ar1"
+  recruitment_how = rbind(as.matrix("none"), as.matrix("none"))
+) 
+
+ecov2 <- list(
+  label = c("lgCopeSpring2", "smCopeSepFeb2"),
+  mean = cbind(as.matrix(log(lgcope.dat$her_sp_Estimate)), 
+               as.matrix(log(smcope.dat$her_larv_Estimate))),
+  logsigma = c("est_1", "est_1"), 
+  year = lgcope.dat$Time,
+  use_obs =  cbind(matrix(1, ncol=1, nrow=dim(lgcope.dat)[1]), use.obs), # use all obs (all = 1)
+  process_model = c("ar1","ar1"), # "rw" or "ar1"
+  recruitment_how = rbind(as.matrix("none"), as.matrix("none"))
+) 
+
+ecov3 <- list(
+  label = c("lgCopeSpring2", "smCopeSepFeb2"),
+  mean = cbind(as.matrix(log(lgcope.dat$her_sp_Estimate)), 
+               as.matrix(log(smcope.dat$her_larv_Estimate))),
+  logsigma = c("est_1", "est_1"), 
+  year = lgcope.dat$Time,
+  use_obs =  cbind(matrix(1, ncol=1, nrow=dim(lgcope.dat)[1]), use.obs), # use all obs (all = 1)
+  process_model = c("rw","rw"), # "rw" or "ar1"
+  recruitment_how = rbind(as.matrix("controlling-lag-0-linear"), as.matrix("controlling-lag-1-linear"))
+) 
+
+ecov4 <- list(
+  label = c("lgCopeSpring2", "smCopeSepFeb2"),
+  mean = cbind(as.matrix(log(lgcope.dat$her_sp_Estimate)), 
+               as.matrix(log(smcope.dat$her_larv_Estimate))),
+  logsigma = c("est_1", "est_1"), 
+  year = lgcope.dat$Time,
+  use_obs =  cbind(matrix(1, ncol=1, nrow=dim(lgcope.dat)[1]), use.obs), # use all obs (all = 1)
+  process_model = c("ar1","ar1"), # "rw" or "ar1"
+  recruitment_how = rbind(as.matrix("controlling-lag-0-linear"), as.matrix("controlling-lag-1-linear"))
+)
+
+# run mod 1
+ecovinput <- set_ecov(input, ecov=ecov1)
+
+mod <- fit_wham(ecovinput, do.osa = T)
+
+saveRDS(mod, file.path(write.dir, "m1.rds"))
+
+plot_wham_output(mod=mod, dir.main=file.path(write.dir,"m1"), out.type='html')
+
+# run mod 2
+ecovinput <- set_ecov(input, ecov=ecov2)
+
+mod <- fit_wham(ecovinput, do.osa = T)
+
+saveRDS(mod, file.path(write.dir, "m2.rds"))
+
+plot_wham_output(mod=mod, dir.main=file.path(write.dir,"m2"), out.type='html')
+
+# run mod 3
+ecovinput <- set_ecov(input, ecov=ecov3)
+
+mod <- fit_wham(ecovinput, do.osa = T)
+
+saveRDS(mod, file.path(write.dir, "m3.rds"))
+
+plot_wham_output(mod=mod, dir.main=file.path(write.dir,"m3"), out.type='html')
+
+# run mod 4
+ecovinput <- set_ecov(input, ecov=ecov4)
+
+mod <- fit_wham(ecovinput, do.osa = T)
+
+saveRDS(mod, file.path(write.dir, "m4.rds"))
+
+plot_wham_output(mod=mod, dir.main=file.path(write.dir,"m4"), out.type='html')
